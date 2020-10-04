5ff707ceb88eab2b353a913e3835c9d858b19eb5
Ilie <ilie.chukitu@gmail.com>
2020-10-04 21:50:29 +0300

    - Made it more beautiful.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
 import { ThemeStore } from './src/stores/theme-store/theme-store';
-import { Provider, useSelector } from 'react-redux';
-import { DarkTheme, DefaultTheme } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
 import 'react-native-gesture-handler';
-import { SafeAreaView } from "react-native-safe-area-context"
 import NavigationWrapper from "./src/NavigationWrapper";
 
 export default () => {
